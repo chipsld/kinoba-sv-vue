e4ad5f7d8377beb79a2cb318697948cad9b8fdbb
Etienne <etienne@kinoba.fr>
2026-04-30 14:05:25 +0200

    feat: add user list test impact

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,10 @@ import react from "@astrojs/react";
 import vue from "@astrojs/vue";
 
 export default defineConfig({
+  image: {
+    domains: ["randomuser.me"],
+  },
+
   i18n: {
     locales: ["fr","es", "en"],
     defaultLocale: "fr",
